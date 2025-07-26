479ad59ec9b0f4bfe76d55a694d1ba1ccb2547c3
ajmal-m <ajua76774@gmail.com>
2025-07-26 15:07:54 +0530

    Admin validation added

--- middleware/Auth.middleware.js
+++ middleware/Auth.middleware.js
@@ -22,3 +22,27 @@ module.exports.protectedRoute = async (req, res, next) => {
         res.status(500).json({ message:"Internal Server Error"});
     }
 }
+
+module.exports.adminValidation = async (req, res, next) => {
+    try {
+        const token = req.cookies.refreshToken;
+
+        if(!token) return res.status(401).json({ message:"Unauthorized - No Token Provided"});
+        const decoded = jwt.verify(token, process.env.REFRESH_SECRET);
+        if(!decoded){
+           return res.status(401).json({message:"Unauthorized - Invalid Token"});
+        }
+        const user = await User.findById(decoded.userId).select("-password");
+        
+        if(!user){
+            return res.status(404).json({ message:"User is not found"})
+        }
+        req.user = user;
+        if(user.role !== 'admin'){
+            return res.status(403).json({ message:"Forbidden - Admin Access Required"});
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({ message:"Internal Server Error"});
+    }
+}
--- routes/User.router.js
+++ routes/User.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { updateUserRole, getAllUsers} = require('../controllers/User.controller');
 const router = express.Router();
+const {adminValidation} = require('../middleware/Auth.middleware');
 
-router.get('/', getAllUsers);
-router.put('/:id/role', updateUserRole);
+router.get('/', adminValidation, getAllUsers);
+router.put('/:id/role', adminValidation, updateUserRole);
 
 
 module.exports = router;
